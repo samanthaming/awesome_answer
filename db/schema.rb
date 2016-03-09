761eacd0a1a764c965eb31a9d7cb2f0eaab51404
Samantha <samantha.ming@gmail.com>
2016-03-08 17:33:13 -0800

    setup DelayedJob and update mailer to use it "deliver_later"
    how to setup DelayedJob:
    1. gem 'delayed_job_active_record'
    2. bin/rails g delayed_job:active_record
    3. bin/rake db:migrate
    3. configure ActiveJob to use DelayedJob in config/application.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308045557) do
+ActiveRecord::Schema.define(version: 20160309011009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,22 @@ ActiveRecord::Schema.define(version: 20160308045557) do
 
   add_index "comments", ["answer_id"], name: "index_comments_on_answer_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"

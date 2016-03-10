40b3a15d434add4d7c724cbe788c3585302db21a
Samantha <samantha.ming@gmail.com>
2016-03-09 20:57:15 -0800

    ajaxify like create and deletion

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,22 +2,30 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    q = Question.find params[:question_id]
-    like = Like.new(question: q, user: current_user)
+    @question = Question.find params[:question_id]
+    like = Like.new(question: @question, user: current_user)
 
-    if like.save
-      redirect_to q, notice: "Liked!"
-    else
-      redirect_to q, alert: "Not Liked!"
+    respond_to do |format|
+      if like.save
+        format.html{redirect_to @question, notice: "Liked!"}
+        format.js{ render :create_success, layout: false }
+      else
+        format.html{redirect_to @question, alert: "Not Liked!"}
+        format.js{ render :create_failure, layout: false }
+      end
     end
 
   end
 
   def destroy
-    q = Question.find params[:question_id]
+    @question = Question.find params[:question_id]
     like = current_user.likes.find params[:id]
     like.destroy
-    redirect_to q, notice: "Un-Liked!"
+
+    respond_to do |format|
+      format.html{redirect_to @question, notice: "Un-Liked!"}
+      format.js{ render :destroy, layout: false }
+    end
   end
 
 end

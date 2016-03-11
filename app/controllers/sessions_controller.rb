d7d5b3eb1ea4424ea154c4d22f4b73eaa78237ab
Samantha <samantha.ming@gmail.com>
2016-03-10 18:57:19 -0800

    convert sigin using simple form
    - using symbol in place of the model and adjusting controller to suit. (first argument of Simple Form)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email params[:email]
+    # b/c using a symbol in place of the first arugment in Simple Form,
+    #  so params are now within [:session] <it can be whatever you name it>
+    @user = User.find_by_email params[:session][:email]
 
-    if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:session][:password])
       sign_in(@user)
       redirect_to root_path, notice: "Signed in"
     else
